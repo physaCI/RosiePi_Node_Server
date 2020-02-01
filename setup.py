a2df8d07faf39141a80a01286355cb85c1b0dc48
sommersoft <sommersoft@gmail.com>
2020-02-01 10:42:33 -0600

    add setup.py, requirements, and tests/* that were dropped with move from RosiePiApp

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rosiepi_node_server',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'jinja2',
+        'rq',
+        'redis'
+    ]
+)
