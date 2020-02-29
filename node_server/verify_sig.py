19fc43546bc176e27a2580be730379efea58763a
sommersoft <sommersoft@gmail.com>
2020-02-29 17:49:29 -0600

    verify_sig.py: fix local_sig_hash to point to correct sig_key attr

--- node_server/verify_sig.py
+++ node_server/verify_sig.py
@@ -122,7 +122,7 @@ class VerifySig(PhysaCIConfig):
                       f'host: {request.headers.get("Host", "")}\n'
                       f'date: {request.headers.get("Date", "")}')
         local_sig_hashed = hmac.new(
-            self.config.node_sig_key.encode(),
+            self.node_sig_key.encode(),
             msg=sig_string.encode(),
             digestmod=sha256
         )
